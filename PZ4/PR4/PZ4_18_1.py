0ffcdcef0b4e81559331d007fab84fc156bd2c97
vlad999999999999 <kuzvv7777@gmail.com>
2024-11-22 14:54:14 +0300

    Update PZ4_18_1.py

--- PZ4/PR4/PZ4_18_1.py
+++ PZ4/PR4/PZ4_18_1.py
@@ -6,18 +6,20 @@
 #выражения 1+X+X^2/2!+⋯+X^N/N!. Полученное число является приближенным
 #значением функции exp(X).
 
-import math
+x = float(input("Введите число X: "))
+n = int(input("Введите число N (должно быть больше 0): "))
 
-try:
-    x = float(input("Введите вещественное число X: "))
-    n = int(input("Введите целое число N (> 0): "))
-    if n <= 0:
-        raise ValueError
-except ValueError:
-    print("Неправильный ввод!")
-    exit(1)
+if n <= 0:  # Проверяем, что N больше 0
+    print("Число N должно быть больше 0!")
+    exit()
 
-result = 1
+# Считаем сумму
+result = 1  # Начальное значение суммы
 for i in range(1, n + 1):
-    result += x**i / math.factorial(i)
-print(f"Приближенное значение функции exp в точке X: {result}")
+    faktorial = 1
+    for j in range(1, i + 1):  # Считаем факториал вручную
+        faktorial *= j
+    result += (x ** i) / faktorial  # Добавляем к результату
+
+# Вывод результата
+print("Приближённое значение exp(X):", result)
